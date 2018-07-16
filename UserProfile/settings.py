08b7f5d151c6cd815a79737a6e1d2898ef810de6
Milan Kathiriya <milankathiriya99@gmail.com>
2018-07-16 09:56:52 +0530

    added static/static_prod and modify docker-compose.yml

--- UserProfile/settings.py
+++ UserProfile/settings.py
@@ -126,5 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/static_dep/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_prod')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static', 'static_dev')]
